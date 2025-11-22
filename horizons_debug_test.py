6fa4d6ed907fddd5b5076c73e9c2c96a68030c6f
widooerlemans <widooerlemans@hotmail.com>
2025-11-22 18:07:35 +0100

    Delete horizons_debug_test.py

--- horizons_debug_test.py
+++
@@ -1,8 +0,0 @@
-from astroquery.jplhorizons import Horizons
-
-try:
-    obj = Horizons(id='1P/Halley', id_type='designation', location='500')
-    eph = obj.ephemerides()
-    print("SUCCESS: HALLEY returned", eph[0])
-except Exception as e:
-    print("FAILURE:", e)
